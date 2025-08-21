7ce2ef71862a2334d6dbed5ba24941ec7a7b65a9
Phat <thanhphatggclass@gmail.com>
2025-08-21 19:37:44 +0700

    new branch for integral test

---
+++ integral_test.c
@@ -0,0 +1,10 @@
+#include <MY_LIB/global_var.h>
+#include <MY_LIB/my_calculator.h>
+#include <MY_LIB/inputsDisplays.h>
+#include <gsl/integration/gsl_integration.h>
+
+
+int main()
+{
+    return 0;
+}
