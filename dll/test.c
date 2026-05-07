1b4721b0d526b1ffb326612dde8ff2ddadd857ed
Phat <thanhphatggclass@gmail.com>
2026-05-07 17:03:15 +0700

    dll test

---
+++ dll/test.c
@@ -0,0 +1,14 @@
+#include <MY_LIB/types_and_specifiers.h>
+#include <MY_LIB/support_function.h>
+#include <MY_LIB/extra_math_function.h>
+#include <MY_LIB/my_calculator.h>
+
+int main(int argc, char const *argv[])
+{
+    printf("Hello");
+    return 0;
+}
+
+
+
+
